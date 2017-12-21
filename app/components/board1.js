d2b0f473c941fbf0d62b4b666c47cad9f9eadeb5
yevheniiKravtsov <y.k.kravtsov@gmail.com>
2017-12-21 13:32:20 +0100

    add card without modal

--- app/components/board1.js
+++ app/components/board1.js
@@ -223,6 +223,7 @@ class App extends React.Component {
     const onCardClick = (cardId,metadata,laneId) => {
       idOfCard=cardId;
       idOfLane=laneId
+      
       var nextState = this.state.lanes;
       for(var i=0;i<nextState.length;i++){
         for(var j=0;j<nextState[i].cards.length;j++){
@@ -404,19 +405,14 @@ class App extends React.Component {
             title: "Card"+(idForNewCard+"."+i),
             ListofComments:[]});
             idOfCard="Card"+(idForNewCard+"."+i);
-            this.state.nameOfCard=idOfCard
-            this.state.DescriptionOfCard=''
-            this.pirntListOfComments();
+
           }
 
         }
-         //nextState.push();
-         //$('#cardId').val(cardId);
 
 
-         $('#cardId').val(idOfCard);
-         $('#myModal2').modal('show');
-         //nextState.push();
+
+
          this.setState(this.state.lanes);
          var Url = 'http://localhost:9080/myapp/boards/';
           Url += this.props.match.params.id;
